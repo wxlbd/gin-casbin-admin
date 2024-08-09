095ac6aba134d51657eb6deacb9b1f650dc83398
wxl <297750182@qq.com>
2024-08-09 15:38:53 +0800

    feat: 角色增加标签字段，标签字段唯一

--- internal/model/role.go
+++ internal/model/role.go
@@ -8,7 +8,8 @@ import (
 // AdminRole 后台角色
 type AdminRole struct {
 	Id          int    `json:"id"`
-	Name        string `json:"name" gorm:"type:VARCHAR(20);not null;comment:'角色名称'';uniqueIndex:uidx_role_name;"`
+	Name        string `json:"name" gorm:"type:VARCHAR(20);not null;comment:'角色名称''"`
+	Tag         string `json:"tag" gorm:"type:VARCHAR(20);NOT NULL;DEFAULT:'';uniqueIndex:uidx_role_name;"`
 	Status      int    `json:"status" gorm:"type:TINYINT;NOT NULL;DEFAULT:1"`
 	Description string `json:"description" gorm:"type:VARCHAR(255);NOT NULL;DEFAULT:''"`
 	CreatedAt   time.Time
